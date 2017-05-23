2ad7d9d250d24bef4988e85f08430a1901ec7271
Mike Anelli <myke_a@mac.com>
2017-05-22 20:01:22 -0700

    package updates

--- Package.swift
+++ Package.swift
@@ -21,10 +21,8 @@
 import PackageDescription
 
 let package = Package(
-	name: "VaporFileMaker",
-	targets: [],
+	name: "FileMakerConnector",
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-XML.git", majorVersion: 2)
-	],
-	exclude: []
+		.Package(url:"https://github.com/PerfectlySoft/Perfect-XML.git", majorVersion: 2, minor: 0)
+	]
 )
